45b2a468c6d33fa4570ffe0c09ae95f2e6f07724
Redshark61 <tim.vannier@gmail.com>
2022-06-01 15:18:25 +0200

    [IMP] new states (error, loading)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const [movies, setMovies] = useState([]);
+	const [error, setError] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+	const fetchMoviesHandler = useCallback(async () => {
+		setIsLoading(true);
+		setError(false);
+		setMovies([]);
+		fetch("https://swapi.dev/api/films")
+			.then((res) => res.json())
+			.catch(() => {
+				setError(true);
+				setIsLoading(false);
+			})
+			.then((json) => {
+				setMovies(json.results);
+				setIsLoading(false);
+			});
+	}, []);
+
+	useEffect(() => {
+		fetchMoviesHandler();
+	}, [fetchMoviesHandler]);
+
+	return (
+		<>
+			<section>
+				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+			</section>
+			<section>
+				{!error && !isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+				{!error && isLoading && <h3>Chargement...</h3>}
+				{!error && !isLoading && !movies.length && <h3>Nothing to display</h3>}
+				{error && <h3>Error</h3>}
+			</section>
+		</>
+	);
 }
 
 export default App;
